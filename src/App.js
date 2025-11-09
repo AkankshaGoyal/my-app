e5c8776f5c5c6c682db3ca8e46dfc93f25570f45
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2025-11-09 17:00:33 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
-import Textform from './Components/Textform';
+// import Textform from './Components/Textform';
 
 function App() {
   return (
       <>
       <Navbar title = "TextUtils" />
       <div className = "container">
-      <Textform heading = "Enter the text to analyze" />
+       {/* <Textform heading = "Enter the text to analyze" /> 
+      */}
+      
+      <About/>
        </div>
       </>
   
