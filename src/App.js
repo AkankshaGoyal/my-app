b0def0fa1d184b98439f685beda754fd5c3f53d9
Akanksha Goyal <akankshagoyalju2000@gmail.com>
2025-11-11 17:08:46 +0530

    Prepare for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
-// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import Textform from './Components/Textform';
 
 function App() {
+ 
   return (
       <>
-      <Navbar title = "TextUtils" />
+      <Navbar title = "TextUtils"  />
       <div className = "container">
      <Textform heading = "Enter the text to analyze" /> 
-      
-      
-      {/* <About/> */}
-       </div>
+    </div>
       </>
   
   );
